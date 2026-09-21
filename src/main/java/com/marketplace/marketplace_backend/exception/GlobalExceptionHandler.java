e1b1824b6a167e0b08485e2c9f6ca509090dac08
Orlando-Diaz <orlando.diazr@uqvirtual.edu.co>
2026-09-21 18:26:47 -0500

    build: agregar dependencias y actualizar configuraciones para correo y reseñas

---
+++ src/main/java/com/marketplace/marketplace_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.marketplace.marketplace_backend.exception;
+
+public class GlobalExceptionHandler {
+}
---
+++ src/main/java/com/marketplace/marketplace_backend/service/ResenaService.java
@@ -0,0 +1,4 @@
+package com.marketplace.marketplace_backend.service;
+
+public class ResenaServic {
+}
